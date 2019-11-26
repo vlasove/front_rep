c737619a0c40a71938d37c50a0c7415294316894
Evgeny Vlasov <evgeny_vlasov@yahoo.com>
2019-11-26 13:19:43 +0300

    Bl

---
+++ third/kek.js
@@ -0,0 +1,42 @@
+let message = "Hello";
+message = 23;
+// 7 types 
+//1. number
+let n = 123.23;
+n = 123;
+let answer = -n /0;
+// alert(answer);
+// alert("hello"/2)
+
+//2. Strings
+let name = "Ivan";
+alert(`Hi, ${name}`);
+
+//3. bool
+let vars = true;
+let anotherVars = 4 > 1;
+
+//4. null == None
+
+
+//5. undefined
+let x;
+alert(x);
+
+alert(1 + true);
+
+"" + 1 + 0 // 10
+"" - 1 + 0 // -1
+true + false // 1
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px"// 45px --- 9[X]
+"$" + 4 + 5 // $45
+"4" - 2 //2
+"4px" - 2 // NaN
+7 / 0 // Infinity
+"  -9  " + 5 //"  -9  5"
+"  -9  " - 5 //-14
+null + 1 // 1
+undefined + 1 // NaN
+" \t \n" - 2 // -2
